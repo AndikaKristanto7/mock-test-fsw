4e2954b4a65f7bfaa9f20cc916df35188f87d161
AndikaKristanto7 <andikakristanto789@gmail.com>
2024-05-23 13:32:22 +0700

    [FE] Set Submit Resp to default when back is clicked

--- to-do-list/src/views/Login.js
+++ to-do-list/src/views/Login.js
@@ -78,6 +78,9 @@ function App() {
         setCheckUsernameResp(
             obj
         )
+        setSubmitResp(
+            obj
+        )
     }
     const onSubmit = async (data) => {
         let submitRespObj = {isSubmitted : true}
